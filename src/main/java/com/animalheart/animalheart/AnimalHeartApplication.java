cf099901092a0def200908c0706e082ab19a9ff6
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-11-15 13:44:05 -0600

    first init

---
+++ src/main/java/com/animalheart/animalheart/AnimalHeartApplication.java
@@ -0,0 +1,13 @@
+package com.animalheart.animalheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHeartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalHeartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/animalheart/animalheart/AnimalHeartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalheart.animalheart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalHeartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
