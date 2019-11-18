97f2ac2439a63e55971e59fb8463e30933188f5e
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-18 16:35:15 -0600

    add helpful commands

---
+++ helpful-commands.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS animal_heart_db;
+
+USE animal_heart_db;
+
+SHOW TABLES;
+
+SELECT * FROM users;
+
+SELECT * FROM followers;
+
+SELECT * FROM events;
+
+SELECT * FROM organization_profiles;
+
+SELECT * FROM user_profiles;
+
+SELECT * FROM animal;
+
+SELECT * FROM comments;
+
+
+
