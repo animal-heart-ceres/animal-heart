73b147cd8bffabe55a34b719349d39f645ba0d37
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-19 21:01:49 -0600

    remove th thymeleaf markup for href

--- src/test/java/com/animalheart/animalheart/UsersIntegrationTest.java
+++ src/test/java/com/animalheart/animalheart/UsersIntegrationTest.java
@@ -25,75 +25,68 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class UsersIntegrationTest {
 
-//    private User testUser;
-//    private User testOrganization;
-//    private HttpSession httpSession;
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Autowired
-//    UserRepository userDao;
-//
-//
-////    @Autowired
-////    private PasswordEncoder passwordEncoder;
-//
-//    @Before
-//    public void setup() throws Exception {
-//
-//        testUser = userDao.findByUsername("testUser");
-//        testOrganization = userDao.findByUsername("testOrganization");
-//
-//        // Creates the test user if not exists
-//        if(testUser == null){
-//            User newUser = new User();
-//            newUser.setUsername("testUser");
-//            newUser.setPassword("password");
-//            newUser.setEmail("testUser@codeup.com");
-//            newUser.setAdmin(true);
-//            newUser.setOrganization(false);
-//            testUser = userDao.save(newUser);
-//        }
-//
-//        if(testOrganization == null){
-//            User newUser = new User();
-//            newUser.setUsername("testOrganization");
-//            newUser.setPassword("passwordOrg");
-//            newUser.setEmail("testOrganization@codeup.com");
-//            newUser.setAdmin(false);
-//            newUser.setOrganization(true);
-//            testOrganization = userDao.save(newUser);
-//        }
-//
-//
-//    }
-//
-//    @Test
-//    public void testCreateUser() throws Exception {
-//        // Makes a Post request to /sign-up and expect a redirection to the home
-//        this.mvc.perform(
-//                post("/sign-up")
-////                        .session((MockHttpSession) httpSession)
-//                        // Add all the required parameters to your request like this
-//                        .param("username", "testUserSignUp")
-//                        .param("email", "testUserSignUp@email.com")
-//                        .param("password", "testUserSignUpPassword")
-////                        .param("isAdmin", "true")
-////                        .param("isOrganization", "false")
-//        )
-//                .andExpect(status().is3xxRedirection());
-//    }
+    private User testUser;
+    private User testOrganization;
+    private HttpSession httpSession;
 
-//    @Test
-//    public void testCreateUserProfile() throws Exception {
-//        this.mvc.perform(
-//                post("/create-user-profile")
-//                .param("firstName", "testFirstName")
-//                .param("lastName", "testLastName")
-//                .param("address", "testAddress"))
-//
-//                .andExpect(status().is3xxRedirection());
-//    }
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    UserRepository userDao;
+
+    @Before
+    public void setup() throws Exception {
+
+        testUser = userDao.findByUsername("testUser");
+        testOrganization = userDao.findByUsername("testOrganization");
+
+        // Creates the test user if not exists
+        if(testUser == null){
+            User newUser = new User();
+            newUser.setUsername("testUser");
+            newUser.setPassword("password");
+            newUser.setEmail("testUser@codeup.com");
+            newUser.setAdmin(true);
+            newUser.setOrganization(false);
+            testUser = userDao.save(newUser);
+        }
+
+        if(testOrganization == null){
+            User newUser = new User();
+            newUser.setUsername("testOrganization");
+            newUser.setPassword("passwordOrg");
+            newUser.setEmail("testOrganization@codeup.com");
+            newUser.setAdmin(false);
+            newUser.setOrganization(true);
+            testOrganization = userDao.save(newUser);
+        }
+    }
+
+    @Test
+    public void testCreateUser() throws Exception {
+        // Makes a Post request to /sign-up and expect a redirection to the home
+        this.mvc.perform(
+                post("/sign-up")
+                        .param("username", "newTestUserSignUp")
+                        .param("email", "newTestUserSignUp@email.com")
+                        .param("password", "newTestUserSignUpPassword")
+        )
+                .andExpect(status().is3xxRedirection());
+
+        User existingUser = userDao.findByUsername("newTestUserSignUp");
+        System.out.println("USER WAS CREATED " + existingUser.getUsername());
+        userDao.delete(existingUser);
+    }
+
+    @Test
+    public void testCreateUserProfile() throws Exception {
+        this.mvc.perform(
+                post("/create-user-profile")
+                .param("firstName", "testFirstName")
+                .param("lastName", "testLastName")
+                .param("address", "testAddress"))
+                .andExpect(status().is3xxRedirection());
+    }
 
 }
