db507db7d6b5dc4e95fd7c8ad9625f111e04967e
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-12-01 23:01:50 -0600

    added google map and got the addresses to show in console so far

--- src/main/java/com/animalheart/animalheart/controllers/MapController.java
+++ src/main/java/com/animalheart/animalheart/controllers/MapController.java
@@ -1,14 +1,31 @@
 package com.animalheart.animalheart.controllers;
 
+import com.animalheart.animalheart.models.UserProfile;
+import com.animalheart.animalheart.repositories.OrganizationProfileRepository;
+import com.animalheart.animalheart.repositories.UserProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MapController {
 
+    @Autowired
+    UserProfileRepository userProfileDao;
+
+    @Autowired
+    OrganizationProfileRepository organizationProfileDao;
+
+
     @GetMapping("/map")
-    public String index() {
+    public String index(Model vModel) {
+        vModel.addAttribute("userProfiles", userProfileDao.findAll());
+        vModel.addAttribute("organizationProfiles", organizationProfileDao.findAll());
         return "map";
     }
+
 
 }
